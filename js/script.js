262d0e17f02e3ba77a390ab1a5c709d93e9a3819
Matteo Tagliatti <matteotagliatti@gmail.com>
2022-02-23 15:48:31 +0100

    add ticketType and small UI tweak

--- js/script.js
+++ js/script.js
@@ -7,13 +7,16 @@ generaButton.addEventListener("click", function () {
 
   let ticketCost = 0.27 * kmToGo;
   let message = "";
+  let ticketType = "Biglietto standard";
 
   if (age === "minorenne") {
     ticketCost = ticketCost - ticketCost * 0.17;
     message = "applicato 17% di sconto minorenni";
+    ticketType = "Biglietto scontato";
   } else if (age === "over") {
     ticketCost = ticketCost - ticketCost * 0.33;
     message = "applicato 33% di sconto over 65";
+    ticketType = "Biglietto scontato";
   }
 
   ticketCost = ticketCost.toFixed(2);
@@ -29,6 +32,7 @@ generaButton.addEventListener("click", function () {
   document.getElementById("messaggio").innerText = message;
   document.getElementById("carrozza").innerText = carriage;
   document.getElementById("codice-cp").innerText = "99" + cpCode;
+  document.getElementById("biglietto").innerText = ticketType;
 
   document.getElementById("second-section").classList.remove("hidden");
   document.getElementById("second-section").classList.add("visible");
