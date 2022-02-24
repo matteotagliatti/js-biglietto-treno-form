20747ac81e6aa1c8a19f20024574257afb67e4f7
Matteo Tagliatti <matteotagliatti@gmail.com>
2022-02-24 10:36:16 +0100

    age in a variable, better initial if

--- js/script.js
+++ js/script.js
@@ -3,11 +3,14 @@ const annullaButton = document.getElementById("annulla");
 const secondSection = document.getElementById("second-section");
 const nameSurname = document.getElementById("nome-cognome");
 const kmToGo = document.getElementById("km");
+const age = document.getElementById("eta");
 
 generaButton.addEventListener("click", function () {
-  const age = document.getElementById("eta").value;
-
-  if (nameSurname.value === "" || kmToGo.value === "") {
+  if (
+    nameSurname.value === "" ||
+    kmToGo.value === "" ||
+    isNaN(parseInt(kmToGo.value))
+  ) {
     alert("Compila correttamente i campi Nome Cognome e Km da percorrere!");
     throw new Error("Campi da compilare");
   }
@@ -16,15 +19,17 @@ generaButton.addEventListener("click", function () {
   let message = "";
   let ticketType = "Biglietto standard";
 
-  if (age === "minorenne") {
+  if (age.value === "minorenne") {
     ticketCost = ticketCost - ticketCost * 0.17;
     message = "applicato 17% di sconto minorenni";
     ticketType = "Biglietto scontato";
-  } else if (age === "over") {
+  } else if (age.value === "over") {
     ticketCost = ticketCost - ticketCost * 0.33;
     message = "applicato 33% di sconto over 65";
     ticketType = "Biglietto scontato";
   }
+
+  ticketCost = ticketCost.toFixed(2);
 
   /* Random carriage */
   const carriage = Math.floor(Math.random() * 9 + 1);
@@ -37,7 +42,7 @@ generaButton.addEventListener("click", function () {
   document.getElementById("messaggio").innerText = message;
   document.getElementById("carrozza").innerText = carriage;
   document.getElementById("codice-cp").innerText = "99" + cpCode;
-  document.getElementById("biglietto").innerText = ticketType.toFixed(2);
+  document.getElementById("biglietto").innerText = ticketType;
 
   secondSection.classList.remove("hidden");
   secondSection.classList.add("visible");
@@ -46,6 +51,7 @@ generaButton.addEventListener("click", function () {
 annullaButton.addEventListener("click", function () {
   nameSurname.value = "";
   kmToGo.value = "";
+  age.value = "maggiorenne";
 
   secondSection.classList.remove("visible");
   secondSection.classList.add("hidden");
